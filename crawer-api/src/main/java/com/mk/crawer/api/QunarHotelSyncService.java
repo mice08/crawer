e4f9d8c14d10c1a827fc9b9bfd1a69599f09a157
康小龙 <kangxiaolong@kangxiaolongdeMacBook-Pro.local>
2016-01-06 16:27:41 +0800

    add alibaba.json

---
+++ crawer-api/src/main/java/com/mk/crawer/api/QunarHotelSyncService.java
@@ -0,0 +1,11 @@
+package com.mk.crawer.api;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Created by kangxiaolong 2016-01-05
+ */
+public interface QunarHotelSyncService {
+    public Map<String,Object> qunarHotelSync();
+}
