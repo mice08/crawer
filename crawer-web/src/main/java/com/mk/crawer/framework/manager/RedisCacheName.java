c962abefe891161d551767538c45c8f00cccf11e
zhentao.deng <zhentao.deng@imike.com>
2016-01-06 17:12:45 +0800

    http util

--- crawer-web/src/main/java/com/mk/crawer/framework/manager/RedisCacheName.java
+++ crawer-web/src/main/java/com/mk/crawer/framework/manager/RedisCacheName.java
@@ -10,4 +10,9 @@ public class RedisCacheName {
      */
     public static final String W_HOTEL_WE_CHAT_AUTHORIZATION_MAP = "w_hotel_we_chat_authorization_map";
 
+    /**
+     * 存储获取其他网站酒店信息的代理IP的MAP的KEY
+     */
+    public static final String CRAWER_PROXY_SERVER_POOL_MAP = "crawer_proxy_server_pool_map";
+
 }
---
+++ crawer-web/src/main/java/com/mk/crawer/framework/proxy/server/ProxyServer.java
@@ -0,0 +1,37 @@
+package com.mk.crawer.framework.proxy.server;
+
+/**
+ * Created by 振涛 on 2016/1/6.
+ */
+public class ProxyServer {
+
+    private String ip;
+
+    private Integer port;
+
+    private Long expirationTime;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public Long getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(Long expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+}
