4b5d612523533dabe10f2ce4daad4f55033bf80b
aarong <maomao.guan@imike.com>
2016-01-07 17:25:54 +0800

    remove unecessary hoteldetail services

--- crawer-biz/src/main/java/com/mk/crawer/biz/mapper/crawer/RoomTypeFacilitiesMapper.java
+++ crawer-biz/src/main/java/com/mk/crawer/biz/mapper/crawer/RoomTypeFacilitiesMapper.java
@@ -1,19 +1,14 @@
 package com.mk.crawer.biz.mapper.crawer;
 
 import com.mk.crawer.biz.model.crawer.RoomTypeFacilities;
-import com.mk.crawer.biz.model.crawer.RoomTypeFacilitiesExample;
-import java.util.List;
 
 public interface RoomTypeFacilitiesMapper {
-    int countByExample(RoomTypeFacilitiesExample example);
 
     int deleteByPrimaryKey(Long id);
 
     int insert(RoomTypeFacilities record);
 
     int insertSelective(RoomTypeFacilities record);
-
-    List<RoomTypeFacilities> selectByExample(RoomTypeFacilitiesExample example);
 
     RoomTypeFacilities selectByPrimaryKey(Long id);
 
