0bf9b90fd7162f439702b3aece26d1084d46df03
zhentao.deng <zhentao.deng@imike.com>
2016-01-07 19:56:11 +0800

    add mike bi proxy

--- framework/src/main/java/com.mk/framework/proxy/http/ProxyServerFetch.java
+++ framework/src/main/java/com.mk/framework/proxy/http/ProxyServerFetch.java
@@ -57,9 +57,36 @@ public class ProxyServerFetch {
         return proxyServerList;
     }
 
+    public static List<ProxyServer> byMikeBI() {
+        List<ProxyServer> proxyServerList = new LinkedList<>();
+
+        try {
+            String ipListStr = HttpUtil.doGetNoProxy(Config.IMIKE_BI_PROXY_IP_LIST_URL);
+
+            String[] ipListStrArray = ipListStr.split("123");
+
+            for (String proxy : ipListStrArray) {
+                String[] temp = proxy.split(":");
+
+                if ( temp.length == 2 ) {
+                    ProxyServer proxyServer = new ProxyServer();
+                    proxyServer.setIp(temp[0]);
+                    proxyServer.setPort(Integer.valueOf(temp[1]));
+
+                    proxyServerList.add(proxyServer);
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.error("获取眯客的代理IP列表出错：", e);
+        }
+
+        return proxyServerList;
+    }
+
     public static void main(String[] args) throws IOException {
 //        List<ProxyServer> proxyServerList = byMike();
-        List<ProxyServer> proxyServerList = byBill();
+//        List<ProxyServer> proxyServerList = byBill();
+        List<ProxyServer> proxyServerList = byMikeBI();
         for (ProxyServer proxyServer : proxyServerList) {
             LOGGER.info("代理IP：{}", JSONUtil.toJson(proxyServer));
         }
