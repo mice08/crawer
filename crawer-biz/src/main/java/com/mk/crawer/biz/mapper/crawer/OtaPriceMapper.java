b967860a4418053d9964f5528ae18668a6a93b0e
kangxiaolong <xiaolong.kang@imike.com>
2016-01-11 20:23:29 +0800

    add ota price

---
+++ crawer-biz/src/main/java/com/mk/crawer/biz/mapper/crawer/OtaPriceMapper.java
@@ -0,0 +1,25 @@
+package com.mk.crawer.biz.mapper.crawer;
+
+import com.mk.crawer.biz.model.crawer.Hotel;
+import com.mk.crawer.biz.model.crawer.HotelExample;
+
+import java.util.List;
+
+public interface OtaPriceMapper {
+    int countByExample(HotelExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Hotel record);
+
+    int insertSelective(Hotel record);
+
+    List<Hotel> selectByExample(HotelExample example);
+
+    Hotel selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Hotel record);
+
+    int updateByPrimaryKey(Hotel record);
+    List<Hotel>  selectByCityName(String cityName);
+}
