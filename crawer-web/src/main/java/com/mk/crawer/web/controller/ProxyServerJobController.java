7313981f3c3608c8f071ec7e1b2a49a617072114
zhentao.deng <zhentao.deng@imike.com>
2016-01-12 13:33:19 +0800

    fix

--- crawer-web/src/main/java/com/mk/crawer/web/controller/ProxyServerJobController.java
+++ crawer-web/src/main/java/com/mk/crawer/web/controller/ProxyServerJobController.java
@@ -1,13 +1,23 @@
 package com.mk.crawer.web.controller;
 
+import com.mk.crawer.biz.model.crawer.QunarHotel;
+import com.mk.crawer.biz.model.crawer.QunarHotelExample;
+import com.mk.crawer.biz.servcie.QunarHotelService;
+import com.mk.crawer.job.impl.HotelInfoRefreshThread;
+import com.mk.framework.AppUtils;
+import com.mk.framework.manager.RedisCacheName;
+import com.mk.framework.proxy.http.JSONUtil;
 import com.mk.framework.proxy.http.ProxyServerJob;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.Jedis;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -41,6 +51,52 @@ public class ProxyServerJobController {
         boolean  success = true;
         hm.put("result",result);
         hm.put("success",success);
+        return new ResponseEntity<Map<String,Object>>(hm, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/add-city-hotel")
+    @ResponseBody
+    public ResponseEntity<Map<String,Object>> addCityHotel(String city) {
+        HashMap hm = new HashMap();
+
+        if (StringUtils.isEmpty(city)) {
+            hm.put("result", "为指定城市");
+            hm.put("success", false);
+        } else {
+            Jedis jedis = null;
+
+            try {
+                QunarHotelExample hotelExample = new QunarHotelExample();
+                hotelExample.createCriteria().andCityNameEqualTo(city);
+
+                QunarHotelService qunarHotelService = AppUtils.getBean(QunarHotelService.class);
+
+                List<QunarHotel> hotelList = qunarHotelService.selectByExample(hotelExample);
+                if (hotelList != null){
+                    for (QunarHotel hotel : hotelList) {
+                        HotelInfoRefreshThread hotelInfoRefreshThread = new HotelInfoRefreshThread();
+                        hotelInfoRefreshThread.setHotelId(hotel.getSourceId());
+
+                        jedis.sadd(RedisCacheName.CRAWER_HOTEL_INFO_REFRESH_THREAD_SET, JSONUtil.toJson(hotelInfoRefreshThread));
+                    }
+                }
+
+                hm.put("result", city+"的"+hotelList.size()+"家酒店添加到价格刷新队列");
+                hm.put("success", true);
+            } catch (Exception e) {
+                hm.put("result", e.toString());
+                hm.put("success", false);
+            } finally {
+                if (jedis != null) {
+                    jedis.close();
+                }
+            }
+
+
+
+        }
+
+
         return new ResponseEntity<Map<String,Object>>(hm, HttpStatus.OK);
     }
 
