79c670d57665fde5478b7d1e77908d19641eb5dc
zhentao.deng <zhentao.deng@imike.com>
2016-01-13 13:14:24 +0800

    fix

--- crawer-web/src/main/java/com/mk/crawer/job/hotel/price/HotelInfoRefreshThread.java
+++ crawer-web/src/main/java/com/mk/crawer/job/hotel/price/HotelInfoRefreshThread.java
@@ -2,12 +2,8 @@ package com.mk.crawer.job.hotel.price;
 
 import com.mk.crawer.biz.servcie.HotelDetailCrawlService;
 import com.mk.framework.AppUtils;
-import com.mk.framework.manager.RedisCacheName;
-import com.mk.framework.proxy.http.JSONUtil;
-import com.mk.framework.proxy.http.RedisUtil;
 import com.mk.framework.proxy.http.ThreadUtil;
 import org.slf4j.Logger;
-import redis.clients.jedis.Jedis;
 
 /**
  * Created by 振涛 on 2016/1/8.
@@ -18,32 +14,26 @@ public class HotelInfoRefreshThread implements Runnable {
 
     @Override
     public void run() {
-        Jedis jedis = RedisUtil.getJedis();
+        while (!SystemStatus.JVM_IS_SHUTDOWN) {
+            HotelDetail hotelDetail = null;
 
-        try {
-            while (!SystemStatus.JVM_IS_SHUTDOWN) {
-                HotelDetail hotelDetail = null;
+            try {
+                hotelDetail = HotelPriceManager.take();
 
-                try {
-                    hotelDetail = HotelPriceManager.take();
+                LOGGER.info("开始刷新酒店:{}的价格", hotelDetail.getHotelId());
 
-                    LOGGER.info("开始刷新酒店:{}的价格", hotelDetail.getHotelId());
+                HotelDetailCrawlService hotelDetailCrawlService = AppUtils.getBean(HotelDetailCrawlService.class);
 
-                    HotelDetailCrawlService hotelDetailCrawlService = AppUtils.getBean(HotelDetailCrawlService.class);
+                hotelDetailCrawlService.crawl(hotelDetail.getHotelId());
 
-                    hotelDetailCrawlService.crawl(hotelDetail.getHotelId());
+                HotelPriceManager.remove(hotelDetail);
 
-                    HotelPriceManager.remove(hotelDetail);
-
-                    LOGGER.info("成功刷新酒店：{}的价格", hotelDetail.getHotelId());
-                } catch (Exception e) {
-                    HotelPriceManager.add(hotelDetail);
-                } finally {
-                    ThreadUtil.sleep(Config.REFRESH_PRICE_INTERVAL_TIME);
-                }
+                LOGGER.info("成功刷新酒店：{}的价格", hotelDetail.getHotelId());
+            } catch (Exception e) {
+                HotelPriceManager.add(hotelDetail);
+            } finally {
+                ThreadUtil.sleep(Config.REFRESH_PRICE_INTERVAL_TIME);
             }
-        } finally {
-            RedisUtil.close(jedis);
         }
     }
 
