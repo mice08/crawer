c7115bf6d87bcb46f447ddabd1fe54605c0f0539
zhentao.deng <zhentao.deng@imike.com>
2016-01-13 11:26:55 +0800

    fix

--- framework/src/main/java/com.mk/framework/manager/RedisCacheName.java
+++ framework/src/main/java/com.mk/framework/manager/RedisCacheName.java
@@ -6,24 +6,24 @@ package com.mk.framework.manager;
 public class RedisCacheName {
 
     /**
-     * 存储微信授权信息的MAP的KEY
-     */
-    public static final String W_HOTEL_WE_CHAT_AUTHORIZATION_MAP = "w_hotel_we_chat_authorization_map";
-
-    /**
-     * 存储获取其他网站酒店信息的代理IP的SET的KEY
-     */
-    public static final String CRAWER_PROXY_SERVER_POOL_SET = "crawer_proxy_server_pool_set";
-
-    /**
-     * 存储获取其他网站酒店信息的代理IP的SET的KEY
+     * 存储被加入黑名单的代理IP的SET的KEY
      */
     public static final String CRAWER_BAD_PROXY_SERVER_POOL_SET = "crawer_bad_proxy_server_pool_set";
 
-
+    /**
+     * 待刷新酒店价格所在城市的SET
+     */
     public static final String CRAWER_CITY_NAME_SET = "crawer_city_list_and_city_name";
 
-    public static final String CRAWER_HOTEL_INFO_REFRESH_THREAD_SET = "crawer_hotel_info_refresh_thread_set";
+    /**
+     * 待刷新酒店价格的酒店ID列表
+     */
+    public static final String CRAWER_HOTEL_INFO_REFRESH_SET = "crawer_hotel_info_refresh_set";
+
+    /**
+     * 正在刷新酒店价格的酒店ID列表
+     */
+    public static final String CRAWER_HOTEL_INFO_REFRESHING_SET = "crawer_hotel_info_refreshing_set";
 
 
 }
