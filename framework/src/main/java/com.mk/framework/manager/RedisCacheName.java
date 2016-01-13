a9e02945e45752ccc1304267e90d4c2eed6b64cc
zhentao.deng <zhentao.deng@imike.com>
2016-01-13 14:24:34 +0800

    fix

--- framework/src/main/java/com.mk/framework/manager/RedisCacheName.java
+++ framework/src/main/java/com.mk/framework/manager/RedisCacheName.java
@@ -8,22 +8,22 @@ public class RedisCacheName {
     /**
      * 存储被加入黑名单的代理IP的SET的KEY
      */
-    public static final String CRAWER_BAD_PROXY_SERVER_POOL_SET = "crawer_bad_proxy_server_pool_set";
+    public static final String CRAWER_BAD_PROXY_SERVER_POOL_SET = "CRAWER:BAD:PROXY_SERVER_POOL:SET";
 
     /**
      * 待刷新酒店价格所在城市的SET
      */
-    public static final String CRAWER_CITY_NAME_SET = "crawer_city_list_and_city_name";
+    public static final String CRAWER_CITY_NAME_SET = "CRAWER:CITY:NAME:SET";
 
     /**
      * 待刷新酒店价格的酒店ID列表
      */
-    public static final String CRAWER_HOTEL_INFO_REFRESH_SET = "crawer_hotel_info_refresh_set";
+    public static final String CRAWER_HOTEL_INFO_REFRESH_SET = "CRAWER:HOTEL:INFO:REFRESH:SET";
 
     /**
      * 正在刷新酒店价格的酒店ID列表
      */
-    public static final String CRAWER_HOTEL_INFO_REFRESHING_SET = "crawer_hotel_info_refreshing_set";
+    public static final String CRAWER_HOTEL_INFO_REFRESHING_SET = "CRAWER:HOTEL:INFO:REFRESHING:SET";
 
     /********动态价格-OTA价格***********/
     public static final String DYNAMIC_PRICE_OTA = "DYNAMIC:PRICE:OTA";
