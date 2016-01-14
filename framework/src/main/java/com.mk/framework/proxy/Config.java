7e84b9bb88b8302b07b533693815ae127a7af630
zhentao.deng <zhentao.deng@imike.com>
2016-01-14 21:08:09 +0800

    fix

--- framework/src/main/java/com.mk/framework/proxy/Config.java
+++ framework/src/main/java/com.mk/framework/proxy/Config.java
@@ -28,8 +28,19 @@ public class Config {
     public static final String IMIKE_PROXY_IP_LIST_URL = "http://dev-bypass.imike.cn/ip.json";
     /**
      * 付费IP代理提取地址
+     * api.goubanjia.com
      */
-    public static final String BILL_PROXY_IP_LIST_URL = "http://api.goubanjia.com/api/get.shtml?order=7fad4d177bdcd13da3ae957aef770bd5&num=10000&carrier=0&protocol=0&an1=1&an2=2&sp1=1&sp2=2&sp3=3&sort=1&distinct=0&rettype=0&seprator=%0D%0A&f_loc=1&f_anoy=1&f_speed=1";
+    public static final String BILL_PROXY_IP_API_GBJ = "http://api.goubanjia.com/api/get.shtml?order=7fad4d177bdcd13da3ae957aef770bd5&num=10000&carrier=0&protocol=0&an1=1&an2=2&sp1=1&sp2=2&sp3=3&sort=1&distinct=0&rettype=0&seprator=%0D%0A&f_loc=1&f_anoy=1&f_speed=1";
+    /**
+     * 付费IP代理提取地址
+     * svip.kuaidaili.com
+     */
+    public static final String BILL_PROXY_IP_API_KDL = "http://svip.kuaidaili.com/api/getproxy/?orderid=985277419450057&num=9999&protocol=1&method=1&an_an=1&an_ha=1&format=json&sep=1";
+    /**
+     * 付费IP代理提取地址
+     * svip.kuaidaili.com
+     */
+    public static final String BILL_PROXY_IP_API_DL666 = "http://xvre.daili666api.com/ip/?tid=556875355340186&num=10000";
     /**
      * 每次通过API获取代理IP的时间间隔，单位：毫秒
      */
