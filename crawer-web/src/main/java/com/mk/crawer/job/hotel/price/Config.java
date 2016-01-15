a6816888a2c4b69a12af6df07c5d99cadc458e44
zhentao.deng <zhentao.deng@imike.com>
2016-01-15 18:49:33 +0800

    fix

--- crawer-web/src/main/java/com/mk/crawer/job/hotel/price/Config.java
+++ crawer-web/src/main/java/com/mk/crawer/job/hotel/price/Config.java
@@ -8,7 +8,7 @@ public class Config {
     /**
      * 中国热门城市100个集合的线程池并发数
      */
-    public static final Integer HOT_CITY_100_CONCURRENCY_THREAD_COUNT = 1000;
+    public static final Integer HOT_CITY_100_CONCURRENCY_THREAD_COUNT = 2000;
     /**
      * 中国热门城市1000个集合的线程池并发数
      */
