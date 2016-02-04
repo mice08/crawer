67b9f3ea6416e321f293e7b7df4c4f5895c8a3d0
kirin.li <kirin.li@dianping.com>
2016-02-04 11:43:23 +0800

    fix

--- crawer-biz/src/main/java/com/mk/crawer/biz/servcie/impl/HotelDetailCrawlServiceImpl.java
+++ crawer-biz/src/main/java/com/mk/crawer/biz/servcie/impl/HotelDetailCrawlServiceImpl.java
@@ -91,7 +91,7 @@ public class HotelDetailCrawlServiceImpl implements HotelDetailCrawlService {
 		String strNextDay = DateUtils.getStringFromDate(DateUtils.addDays(day, 1), DateUtils.FORMATSHORTDATETIME);
 
 		String hotelid = hotelIds.get(0);
-
+		hotelImageService.crawl(hotelid);
 		logger.info("++++++++++++++++++++++++bengin crawl hotel" + hotelid + " ++++++++++++++++++");
 		String invokeUrl = String.format(hotelDetailUrl, strCurDay, strNextDay, hotelid);
 
@@ -110,7 +110,7 @@ public class HotelDetailCrawlServiceImpl implements HotelDetailCrawlService {
 
 		processResult(hotelid, jsonString);
 
-		hotelImageService.crawl(hotelid);
+
 	}
 
 	private void processResult(String hotelid, String jsonString) throws Exception {
