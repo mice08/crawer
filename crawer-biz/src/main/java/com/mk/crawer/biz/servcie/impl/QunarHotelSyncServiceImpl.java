e6bc39a7a99605bf0ae65515a189330dfde49809
kirin.li <kirin.li@dianping.com>
2016-02-05 14:56:19 +0800

    fix

--- crawer-biz/src/main/java/com/mk/crawer/biz/servcie/impl/QunarHotelSyncServiceImpl.java
+++ crawer-biz/src/main/java/com/mk/crawer/biz/servcie/impl/QunarHotelSyncServiceImpl.java
@@ -154,9 +154,11 @@ public class QunarHotelSyncServiceImpl implements QunarHotelSyncService {
 
                 example.createCriteria().andValidEqualTo("T");
                 Integer  hotelCount= hotelMappingService.countByExample(example);
-                if (hotelCount==null || hotelCount == 0){
+                if (hotelCount!=null && hotelCount > 0){
                     hotelImageService.crawl(qunarHotel.getSourceId(), false);
                     Thread.currentThread().sleep(1000);
+                }else {
+                    System.out.println("酒店 id" + qunarHotel.getSourceId() +" 不在上线范围内");
                 }
             } catch (InterruptedException e) {
             e.printStackTrace();
